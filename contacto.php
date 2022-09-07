60159ed1f68627ef724e697e8e5c1988caf3b9b5
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-09-06 20:25:54 -0500

    Envío de correo

---
+++ contacto.php
@@ -0,0 +1,27 @@
+<?php
+  if (isset($_POST('enviar'))) {
+      if (!empty($_POST['nombre']) && !empty($_POST['correo']) && !empty($_POST['mensaje'])) {
+         $nombre = $_POST['nombre'];
+         $correo = $_POST['correo'];
+         $mensaje = $_POST['mensaje'];
+
+         $header = "Form: noreply@example.com"."\r\n";
+         $header .= "Reply-To: noreply@example.com"."\r\n";
+         $header .= "X-Mailer: PHP/".phpversion();
+
+         $asunto = "Hola! cordial saludo. Queremos agradecerte por habernos enviado tu opinion acerca de nuestro sitio web. Te comentamenos, que dentro de muy poco nos comunicaremos contigo para conocer que otras sugerencias puedes darnos.\r\n\n";
+         $asunto .= "Este mensaje fue enviado por: Josué Romero\r\n";
+         $asunto .= "Tu mensaje: ".$mensaje."\r\n";
+         $asunto .= "Fecha de envío: ".date('d/m/Y', time());
+
+         $de = "josueromram3@gmail.com";
+         $titulo = "¡Gracias por darnos tu opinion!";
+
+         $email = @mail($para, $titulo, $asunto, $header);
+         if ($email) {
+         echo "<h3>¡Correo enviado satisfactoriamente!</h4>";
+         //header("Location:index.html");
+         }
+      }
+   }
+?>

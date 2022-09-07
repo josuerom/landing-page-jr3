9d3a21ffe362b171da05fbc9e6791c33543b3c94
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-09-06 19:50:20 -0500

    Update correo.php

--- correo.php
+++ correo.php
@@ -1,22 +1,25 @@
 <?php
-	$nombre = $_POST['nombre'];
-	$correo = $_POST['correo'];
-	$mensaje = $_POST['mensaje'];
+	if (isset($_POST('enviar'))) {
+		$nombre = $_POST['nombre'];
+		$correo = $_POST['correo'];
+		$mensaje = $_POST['mensaje'];
 
-	$header = 'Form: '.$correo." \r\n";
-	$header .= "X-Mailer: PHP/".phpversion()." \r\n";
-	$header .= "Mime-Version: 1.0 \r\n";
-	$header .= "Content-Type: text/plain";
+		$header = 'Form: noreply@example.com"."\r\n";
+		$header .= "Reply-To: noreply@example.com"."\r\n";
+		$header .= "X-Mailer: PHP/".phpversion();
 	
-	$asunto = "Hola! cordial saludo. Queremos agradecerte por habernos enviado tu opinion acerca de nuestro sitio web. Te comentamenos, que dentro de muy poco nos comunicaremos contigo para conocer que otras sugerencias puedes darnos.\r\n\n";
-	$asunto .= "Este mensaje fue enviado por: Josué Romero\r\n";
-	$asunto .= "Correo: josueromram3@gmail.com\r\n";
-	$asunto .= "Tu mensaje: ".$mensaje."\r\n";
-	$asunto .= "Fecha de envío: ".date('d/m/Y', time());
+		$asunto = "Hola! cordial saludo. Queremos agradecerte por habernos enviado tu opinion acerca de nuestro sitio web. Te comentamenos, que dentro de muy poco nos comunicaremos contigo para conocer que otras sugerencias puedes darnos.\r\n\n";
+		$asunto .= "Este mensaje fue enviado por: Josué Romero\r\n";
+		$asunto .= "Tu mensaje: ".$mensaje."\r\n";
+		$asunto .= "Fecha de envío: ".date('d/m/Y', time());
 
-	$de = "josueromram3@gmail.com";
-	$msj = "¡Gracias por tu opinion!";
+		$de = "josueromram3@gmail.com";
+		$titulo = "¡Gracias por darnos tu opinion!";
 
-	mail($para, $msj, utf8_decode($asunto), $header);
-	header("Location:index.html");
+		$email = @mail($para, $titulo, $asunto, $header);
+		if ($email) {
+			echo "<h3>¡Correo enviado satisfactoriamente!</h4>";
+			header("Location:index.html");
+		}
+	}
 ?>
